ca87d10c0ef5fe112c2ddfa43930da7ed4fb2bdf
aditya-bhuyan <aditya.sunjava@gmail.com>
2021-03-26 13:38:23 +0530

    Lab 6: Database

---
+++ database/migrations/V1__initial_schema.sql
@@ -0,0 +1,14 @@
+USE pages;
+create table pages(
+id bigint(20) not null auto_increment,
+business_name VARCHAR(50),
+address VARCHAR(50),
+category_id bigint(20),
+contact_number VARCHAR(50),
+primary key (id)
+)
+engine = innodb
+default charset = utf8;
+
+
+update user set authentication_string=null, plugin='mysql_native_password',host='localhost' where user='root';
